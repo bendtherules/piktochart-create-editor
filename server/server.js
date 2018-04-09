db7a5aa54abfe654723848483cd4120a7d8e6a5e
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2018-04-09 11:59:29 +0530

    Add CORS header in server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const junk = require( 'junk' );
 let app = express();
 
 app.use( express.static('./') );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // define file name and destination to save
 let storage = multer.diskStorage({
